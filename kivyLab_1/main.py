652e6648875fb220ca708e92d98149bfa199b0cb
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2023-02-21 16:33:26 +0000

    Add files via upload

--- kivyLab_1/main.py
+++ kivyLab_1/main.py
@@ -1,5 +1,23 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+
+
+
+
+class BoxLayoutExample(BoxLayout):
+  pass
+  '''  def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+
+        b1 = Button(text="A")
+        b2 = Button(text="B")
+
+
+        self.add_widget(b1)
+        self.add_widget(b2) '''
 
 class MainWidget(Widget):
     pass
